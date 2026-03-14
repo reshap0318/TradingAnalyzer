e955a8a4f5fd59e8a93eb1f66b79c3a60525bd70
reshap0318 <aldo.reshap@gmail.com>
2026-03-15 04:25:17 +0700

    update port be

--- be/cmd/main/main.go
+++ be/cmd/main/main.go
@@ -27,7 +27,7 @@ func main() {
 
 	// CORS Configuration
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:9001", "http://127.0.0.1:9001"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:9001", "http://127.0.0.1:9001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
